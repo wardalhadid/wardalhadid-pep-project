9f3030bd20a82afffc86c5b99f6d7801939eaa9f
Ward Alhadid <wf.hadid94@gmail.com>
2023-08-06 20:34:37 +0000

    removed accountList

--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,8 +1,5 @@
 package Service;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import DAO.AccountDAO;
 import Model.Account;
 
@@ -16,15 +13,12 @@ public class AccountService {
     public AccountService(AccountDAO accountDAO) {
         this.accountDAO = accountDAO;
     }
-
-    List<Account> accountList = new ArrayList<>();
     
     public Account register(Account account) {
         if (!accountDAO.confirmAccountIdExists(account.getAccount_id())) {
             Account accountToBeRegistered = accountDAO.register(account);
         if(!account.getUsername().equals("") &&
             account.getPassword().length() >= 4) {
-            accountList.add(account);
             return accountToBeRegistered;
         } else {
             return null;
